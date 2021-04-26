d253edf7578a1f95cf67411e281c1a121543bdb5
“Ricky <grickyh@gmail.com>
2021-04-26 07:31:40 +0100

    input display button test

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import Project from "./modules/project"
 import Task from "./modules/task"
 import Goal from "./modules/goal"
 import controlSideNav from "./modules/side-menu-function"
+
+
 // Import objects
 import inbox from "./modules/inbox"
+import {openTaskInput} from "./modules/ui-triggers"
+
 
 // default inbox object to store all tasks
 inbox();
@@ -12,6 +16,8 @@ inbox();
 // collapsable side menu
 controlSideNav();
 
+// open task input 
+openTaskInput();
 
 
 
--- src/modules/ui-triggers.js
+++ src/modules/ui-triggers.js
@@ -1,7 +1,18 @@
 // import Task from "./task"
 // import Project from "./project"
 
+function openTaskInput()
+{
+    const openButton = document.getElementById("open-tf");
+    openButton.addEventListener("click", open);
 
+    function open(){
+        const inputVisibility = document.getElementById("input-container");
+        inputVisibility.style.visibility = "visible";
+    }
+}
+
+export {openTaskInput}
 // export default function newTask()
 // {
 //     const taskInput = document.getElementById("todo-title").value;
